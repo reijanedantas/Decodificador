c53c7fd55ffc572c070640decffb5b1353dc8a29
reijanedantas <120532898+reijanedantas@users.noreply.github.com>
2024-02-22 11:12:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+function btnCriptografar() {}
+
+function criptografar(string) {
+  const resultado = string
+  .replaceAll("e", "enter")
+  .replaceAll("i", "imes")
+  .replaceAll("a", "ai")
+  .replaceAll("o", "obter")
+  .replaceAll("u", "ufat");
+
+  return resultado;
+
+}
+
+let test = "Você conseguiu concluir o desafio";
+
+console.log(criptografar (test));
